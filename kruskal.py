9e12f7d1003cd2ebf96ade81a0f6aaee055edd4e
LucasclFerreira <lucascosta.ferreira@sou.unifal-mg.edu.br>
2023-05-27 16:36:55 -0300

    kruskal estruturado

--- kruskal.py
+++ kruskal.py
@@ -26,18 +26,50 @@ class Grafo:
     
     def kruskal(self):
         # conjunto vazio para inicializar o kruskal
-        conjunto = []
+        conjuntos = []
 
         # preenchendo conjunto com os vertices isolados
-        for vertice in range(self.numero_vertices):
-            conjunto.append(self.grafo[vertice])
-        #print(conjunto)
+        for vertice in self.grafo:
+            conjuntos.append(set([vertice]))
+        print(conjuntos)
 
         arestas = []
-        for _, value in self.grafo.items():
-            arestas = arestas + sorted(value.items(), key=lambda x:x[1])
-        arestasOrdenadas = sorted(arestas, key=lambda x:x[1])
+        for origem, adjacentes in self.grafo.items():
+            for destinatario in adjacentes.items():
+                destinatario = tuple([origem]) + destinatario
+                arestas.append(destinatario)
+
+        arestasOrdenadas = sorted(arestas, key=lambda x:x[2])
         print("Arestas ordenadas:", arestasOrdenadas)
+
+        cont = 0
+        for aresta in arestasOrdenadas:
+            print("CONJUNTO DE V1:", self.conjuntoDe(aresta[0], conjuntos))
+            print("CONJUNTO DE V2:", self.conjuntoDe(aresta[1], conjuntos))
+            if self.conjuntoDe(aresta[0], conjuntos) != self.conjuntoDe(aresta[1], conjuntos):
+                print("os conjuntos são DIFERENTES")
+                cont += 1
+                conjuntos = self.aplicarUniao(aresta[0], aresta[1], conjuntos)
+            print("")
+        print(cont)
+        return conjuntos
+    
+    def aplicarUniao(self, origem, destino, conjuntos):
+        for conjunto in conjuntos:
+            if destino in conjunto:
+                conjuntoDestino = conjunto
+                conjuntos.remove(conjunto)
+
+        for conjunto in conjuntos:
+            if origem in conjunto:
+                conjunto.add(conjuntoDestino)
+        return conjuntos
+
+    def conjuntoDe(self, vertice, conjuntos):
+        for conjunto in conjuntos:
+            if vertice in conjunto:
+                return conjunto
+
         
         
 
@@ -50,16 +82,31 @@ grafo.adicionar_vertice(0)
 grafo.adicionar_vertice(1)
 grafo.adicionar_vertice(2)
 grafo.adicionar_vertice(3)
+grafo.adicionar_vertice(4)
+grafo.adicionar_vertice(5)
+grafo.adicionar_vertice(6)
+grafo.adicionar_vertice(7)
+grafo.adicionar_vertice(8)
 
 # Adicionando arestas
-grafo.adicionar_aresta(0, 1, 2)
-grafo.adicionar_aresta(0, 2, 1)
-grafo.adicionar_aresta(0, 3, 3)
-grafo.adicionar_aresta(3, 2, 4)
-grafo.adicionar_aresta(2, 1, 7)
+grafo.adicionar_aresta(0, 1, 4)
+grafo.adicionar_aresta(0, 7, 8)
+grafo.adicionar_aresta(1, 2, 8)
+grafo.adicionar_aresta(1, 7, 11)
+grafo.adicionar_aresta(2, 3, 7)
+grafo.adicionar_aresta(2, 8, 2)
+grafo.adicionar_aresta(2, 5, 4)
+grafo.adicionar_aresta(3, 5, 14)
+grafo.adicionar_aresta(3, 4, 9)
+grafo.adicionar_aresta(4, 5, 10)
+grafo.adicionar_aresta(5, 6, 2)
+grafo.adicionar_aresta(6, 7, 1)
+grafo.adicionar_aresta(6, 8, 6)
+grafo.adicionar_aresta(7, 8, 7)
 
 # Exibindo o grafo
 grafo.exibir_grafo()
 
 # kruskal
-grafo.kruskal()
+agm = grafo.kruskal()
+print(agm)
